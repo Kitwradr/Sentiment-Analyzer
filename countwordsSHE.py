728838ef5b092770c4a3c44064fc8c871f58e96a
shobhit-coder <shobhit1998@yahoo.co.in>
2018-03-28 23:22:54 +0530

    updating counterwords with user info

--- countwordsSHE.py
+++ countwordsSHE.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 import re
 
 from nltk.corpus import stopwords
+from nltk import word_tokenize, pos_tag
 import string
  
 punctuation = list(string.punctuation)
@@ -17,48 +18,49 @@ stop = stopwords.words('english') + punctuation + ['rt','RT', 'via']
  
 
 positive_vocab = [
-    'good', 'nice', 'great', 'awesome', 'outstanding',
-    'fantastic', 'terrific', ':)', ':-)', 'like', 'love','Happy'
-     #'triumph', 'triumphal', 'triumphant', 'victory'
+	'good', 'nice', 'great', 'awesome', 'outstanding',
+	'fantastic', 'terrific', ':)', ':-)', 'like', 'love','happy','applause'
+	# shall we also include game-specific terms?
+	# 'triumph', 'triumphal', 'triumphant', 'victory', etc.
 ]
 negative_vocab = [
-    'bad', 'terrible', 'crap', 'useless', 'hate', ':(', ':-(','outrageous','unjust'
-     'defeat'
+	'bad', 'terrible', 'crap', 'useless', 'hate', ':(', ':-(','outrageous','unjust','stfu','defeat'
+	# 'defeat', etc.
 ]
 
 
 #print(stop)
 emoticons_str = r"""
-    (?:
-        [:=;] # Eyes
-        [oO\-]? # Nose (optional)
-        [D\)\]\(\]/\\OpP] # Mouth
-    )"""
+	(?:
+		[:=;] # Eyes
+		[oO\-]? # Nose (optional)
+		[D\)\]\(\]/\\OpP] # Mouth
+	)"""
  
 regex_str = [
-    emoticons_str,
-    r'<[^>]+>', # HTML tags
-    r'(?:@[\w_]+)', # @-mentions
-    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
-    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+	emoticons_str,
+	r'<[^>]+>', # HTML tags
+	r'(?:@[\w_]+)', # @-mentions
+	r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+	r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
  
-    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
-    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
-    r'(?:[\w_]+)', # other words
-    r'(?:\S)' # anything else
+	r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+	r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+	r'(?:[\w_]+)', # other words
+	r'(?:\S)' # anything else
 ]
-    
+	
 tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
 emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
  
 def tokenize(s):
-    return tokens_re.findall(s)
+	return tokens_re.findall(s)
  
 def preprocess(s, lowercase=True):
-    tokens = tokenize(s)
-    if lowercase:
-        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
-    return tokens
+	tokens = tokenize(s)
+	if lowercase:
+		tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+	return tokens
  
 
 
@@ -67,64 +69,100 @@ com = defaultdict(lambda : defaultdict(int))
 n_docs=0
 fname = 'bhache.json'
 with open(fname, 'r') as f:
-    count_all = Counter()
-    count_single = Counter()
-    count_stop_single = Counter()
-    for line in f:
-        n_docs+=1
-        tweet = json.loads(line)
-        # Create a list with all the terms
-        terms_all = [term for term in preprocess(tweet['text'])]
-        # Update the counter
-        count_all.update(terms_all)
-        
-        terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
-        count_stop_single.update(terms_stop)
+	count_all = Counter()
+	tweetList =  []
+	count_single = Counter()
+	count_stop_single = Counter()
+	for line in f:
+		tweet = json.loads(line)
+		if 'retweeted_status' in tweet:
+			str1=tweet['retweeted_status']['retweet_count']
+			tweetText = tweet['retweeted_status']['text']
+			id1 = tweet['retweeted_status']['id']
+			tweetList= tweetList+[[str1,id1,tweetText]]
 
-        terms_single = set(terms_all)
-        count_single.update(terms_all)
-        # Count hashtags only
-        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
-        # Count terms only (no hashtags, no mentions)
-        terms_only = [term for term in preprocess(tweet['text'])   if term not in stop and not term.startswith(('#', '@'))] 
-                      # mind the ((double brackets))
-                      # startswith() takes a tuple (not a list) if 
-                      # we pass a list of inputs
+		n_docs+=1
+		
+		# Create a list with all the terms
+		terms_all = [term for term in preprocess(tweet['text'])]
+		# Update the counter
+		count_all.update(terms_all)
+		
+		terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+		count_stop_single.update(terms_stop)
 
-        for i in range(len(terms_only)-1):            
-            for j in range(i+1, len(terms_only)):
-                w1, w2 = sorted([terms_only[i], terms_only[j]])                
-                if w1 != w2:
-                    com[w1][w2] += 1
-        # Print the first 5 most frequent words
-    print(count_stop_single.most_common(20))
+		terms_single = set(terms_all)
+		count_single.update(terms_all)
+		# Count hashtags only
+		terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+		# Count terms only (no hashtags, no mentions)
+		terms_only = [term for term in preprocess(tweet['text'])   if term not in stop and not term.startswith(('#', '@'))] 
+						# mind the ((double brackets))
+						# startswith() takes a tuple (not a list) if 
+						# we pass a list of inputs
 
-
-
-
-    # n_docs is the total n. of tweets
-    p_t = {}
-    p_t_com = defaultdict(lambda : defaultdict(int))
-     
-    for term, n in count_stop_single.items():
-        p_t[term] = n / n_docs
-        for t2 in com[term]:
-            p_t_com[term][t2] = com[term][t2] / n_docs
-
-    #print(p_t)
-    print("\n\n")
-    #print(p_t_com)
+		for i in range(len(terms_only)-1):            
+			for j in range(i+1, len(terms_only)):
+				w1, w2 = sorted([terms_only[i], terms_only[j]])                
+				if w1 != w2:
+					com[w1][w2] += 1
+		
+	print(count_stop_single.most_common(20))
 
 
-    com_max = []
+	tweetList.sort()
+
+
+
+	newlist = tweetList[::-1]
+	idlist= list()
+	toptweets=list()
+	ctr=0
+	for i in newlist:
+		if i[1] not in idlist:
+			idlist.append(i[1])
+
+			toptweets.append(i[2])
+			ctr+=1
+		if ctr>5:
+			break
+
+	for text in toptweets:
+		for x,y in pos_tag(word_tokenize(text)):
+			if y=="JJ" or y=="JJR" or y=="JJS":
+				
+				print("Which category does \""+x+"\" belong to? 1 positive 2 negative 3 none.")
+				op=int(input())
+
+				if op==1 and x not in positive_vocab:
+					positive_vocab.append(x)
+				elif op==2 and x not in negative_vocab:
+					negative_vocab.append(x)
+
+
+	# n_docs is the total n. of tweets
+	p_t = {}
+	p_t_com = defaultdict(lambda : defaultdict(int))
+	 
+	for term, n in count_stop_single.items():
+		p_t[term] = n / n_docs
+		for t2 in com[term]:
+			p_t_com[term][t2] = com[term][t2] / n_docs
+
+	#print(p_t)
+	print("\n\n")
+	#print(p_t_com)
+
+
+	com_max = []
 # For each term, look for the most common co-occurrent terms
-    for t1 in com:
-        t1_max_terms = sorted(com[t1].items(), key=operator.itemgetter(1), reverse=True)[:5]
-        for t2, t2_count in t1_max_terms:
-            com_max.append(((t1, t2), t2_count))
-    # Get the most frequent co-occurrences
-    terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
-    print(terms_max[:5])
+	for t1 in com:
+		t1_max_terms = sorted(com[t1].items(), key=operator.itemgetter(1), reverse=True)[:5]
+		for t2, t2_count in t1_max_terms:
+			com_max.append(((t1, t2), t2_count))
+	# Get the most frequent co-occurrences
+	terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
+	print(terms_max[:5])
 
 
 
@@ -134,32 +172,38 @@ with open(fname, 'r') as f:
 
 
 
-    pmi = defaultdict(lambda : defaultdict(int))
-    for t1 in p_t:
-        for t2 in com[t1]:
-            denom = p_t[t1] * p_t[t2]
-            if denom is not 0:
-                pmi[t1][t2] = math.log((p_t_com[t1][t2] / denom),2)
-     
-    semantic_orientation = {}
-    for term, n in p_t.items():
-        positive_assoc = sum(pmi[term][tx] for tx in positive_vocab)
-        negative_assoc = sum(pmi[term][tx] for tx in negative_vocab)
-        semantic_orientation[term] = positive_assoc - negative_assoc
+	pmi = defaultdict(lambda : defaultdict(int))
+	for t1 in p_t:
+		for t2 in com[t1]:
+			denom = p_t[t1] * p_t[t2]
+			if denom is not 0:
+				pmi[t1][t2] = math.log((p_t_com[t1][t2] / denom),2)
+	 
+	semantic_orientation = {}
+	for term, n in p_t.items():
+		positive_assoc = sum(pmi[term][tx] for tx in positive_vocab)
+		negative_assoc = sum(pmi[term][tx] for tx in negative_vocab)
+		if positive_assoc == negative_assoc:
+			print("EQUAL")
+			print(term+" pos"+str(positive_assoc)+" neg "+str(negative_assoc))
 
-    semantic_sorted = sorted(semantic_orientation.items(), 
-                             key=operator.itemgetter(1), 
-                             reverse=True)
-    top_pos = semantic_sorted[:20]
-    top_neg = semantic_sorted[-20:]
+		semantic_orientation[term] = positive_assoc - negative_assoc
 
-    #print(semantic_sorted)
-    print("\ntop negative: ")
-    print(top_neg)
-    print("\nTop positive: ")
-    print(top_pos)
-    print("hazard: %f" % semantic_orientation['hazard'])
+	semantic_sorted = sorted(semantic_orientation.items(), 
+							 key=operator.itemgetter(1), 
+							 reverse=True)
+	top_pos = semantic_sorted[:20]
+	top_neg = semantic_sorted[-20:]
 
+	#print(semantic_sorted)
+	print("\ntop negative: ")
+	print(top_neg)
+	print("\nTop positive: ")
+	print(top_pos)
+	print("HAZARD: "+str(semantic_orientation["hazard"]))
+	print("willian: "+str(semantic_orientation["willian"]))
+	print("conte: "+str(semantic_orientation["conte"]))
+	print("ed: "+str(semantic_orientation["eduardo"]))
 
 
  
